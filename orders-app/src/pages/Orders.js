9cb35f505dfe830a0e5ac5b5e878303dbee42d13
Sicor <atrem641@gmail.com>
2023-11-06 17:22:02 +0300

    fix: modal and orders bug fix

--- orders-app/src/pages/Orders.js
+++ orders-app/src/pages/Orders.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-const Orders = () => {
+
+const Orders = ({orders}) => {
 return (
 <div>
 <h2>Список заказов</h2>
+{orders.map(order => (
+        <OrderItem key={order.id} order={order}/>  
+      ))}
 </div>
 );
 }
+
+const OrderItem = ({order}) => {
+
+    return (
+      <div className="order">
+        <h3>Поставщик : {order.customer}</h3>  
+        <p>Продукт: {order.product}</p>
+        <p>Количиство : {order.count}</p>
+        <p>Состояние : {order.status}</p>
+        <img src={require(`../../public/${order.image}.jpg`)} alt={order.product} />
+      </div>
+    )
+  
+  }
+
+
+export default Orders
