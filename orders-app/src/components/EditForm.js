97d11e028b7713903c3754f20958519aa174e2df
Sicor <atrem641@gmail.com>
2023-12-20 00:41:48 +0300

    added edit form

---
+++ orders-app/src/components/EditForm.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateOrderAction } from '../Redux/actions.js';
+
+const EditForm = ({order}) => {
+
+  const [formData, setFormData] = useState({
+    product: order.product,
+    count: order.count,
+    // и другие поля
+  });
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  }
+
+  const dispatch = useDispatch();
+
+    const handleSubmit = () => {
+    dispatch(updateOrderAction(formData));
+} 
+
+  useEffect(() => {
+    setFormData(order);
+  }, [order]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      
+      <label>
+        Продукт:
+        <input 
+          type="text"
+          name="product"
+          value={formData.product}
+          onChange={handleChange} 
+        />
+      </label>
+
+      <label>
+        Количество: 
+        <input
+          type="number" 
+          name="count"
+          value={formData.count}
+          onChange={handleChange}
+        />  
+      </label>
+
+      // остальные поля
+
+      <button type="submit">Сохранить</button>
+
+    </form>
+  )
+}
+
+export default EditForm
