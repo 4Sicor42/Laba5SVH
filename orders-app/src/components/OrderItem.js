54d46621a64728adcc57b3c0369ea8266cc61690
Sicor <atrem641@gmail.com>
2023-12-19 12:02:07 +0300

    Create OrderItem.js

---
+++ orders-app/src/components/OrderItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ordersitem from '../styles/OrderItem.module.css';
+import OrderPopup from '../components/OrderPopup'
+import OrderImage from '../components/OrderImage'
+
+const OrderItem = ({ order }) => {
+
+return (
+<div className={ordersitem.item}>
+  <OrderPopup 
+    order={order}  
+    />
+
+  <p className={ordersitem}>Продукт: {order.product}</p>
+  <p className={ordersitem}>Количество : {order.count}</p>  
+  <p className={ordersitem}>Состояние : {order.status}</p>
+  <p className={ordersitem}>Цена : {order.price}</p>
+
+  <OrderImage order={order}/>
+
+</div>
+);
+}
+
+export default OrderItem;
