a4fbc934f5af9f257cc3da264f28b56229f79472
Sicor <atrem641@gmail.com>
2023-12-19 12:02:17 +0300

    Update Contacts.js

--- orders-app/src/pages/Contacts.js
+++ orders-app/src/pages/Contacts.js
@@ -1,34 +1,48 @@
 import React from 'react';
+ 
 import styles from '../styles/Contacts.module.css';
+
+
+
+import Reviews from '../components/Reviews';
 
 const Contacts = () => {
   return (
-    <div className={styles.contacts}>
-      
-      <h2>Контактная информация</h2>
+  <div>
 
-      <div className={styles.info}>
+<div className={styles.contacts}>
 
-        <h3>Адрес</h3>
-        <p>г. Москва, ул. Садовая, д.5</p>
+  <h2>Контактная информация</h2>
 
-      </div>
+  <div className={styles.info}>
 
-      <div className={styles.info}>
+    <h3>Адрес</h3>
+    <p>г. Москва, ул. Садовая, д.5</p>
 
-        <h3>Телефон</h3> 
-        <p>+7 (495) 123-45-67</p>
+  </div>
 
-      </div>
+  <div className={styles.info}>
 
-      <div className={styles.info}>
+    <h3>Телефон</h3> 
+    <p>+7 (495) 123-45-67</p>
 
-        <h3>Email</h3>
-        <p>info@company.ru</p>
+  </div>
 
-      </div>
+  <div className={styles.info}>
+
+    <h3>Email</h3>
+    <p>info@company.ru</p>
+
+  </div>
+</div>
+  <div className={styles.reviewsBlock}> 
+    <h3 className={styles.reviewsTitle}> Наши клиенты: </h3> 
+      <div className={styles.feedback}> 
+        <Reviews /> 
+      </div> 
     </div>
+  </div>
   )
-}
-
-export default Contacts;
+  }
+  
+  export default Contacts
