09c945b585d23fda065c295d967420b6d2e866c8
Sicor <atrem641@gmail.com>
2023-12-19 12:01:52 +0300

    Update Header.js

--- orders-app/src/components/Header.js
+++ orders-app/src/components/Header.js
@@ -4,10 +4,10 @@ import styles from '../styles/Header.module.css';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.logo}>Metal Core</h1>
+      <a href="/" className={styles.logo}>Metal Core</a>
       <nav>
         <a href="/">Главная</a>
-        <a href="/orders">Заказы</a>
+        <a href="/orders">Продукты</a>
       </nav>
     </header>
   );
