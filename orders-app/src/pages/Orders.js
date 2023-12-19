c6ef6ef8ebd3f0c7817f906db054d2de0e810b66
Sicor <atrem641@gmail.com>
2023-12-19 12:02:53 +0300

    Updated Orders view

--- orders-app/src/pages/Orders.js
+++ orders-app/src/pages/Orders.js
@@ -1,34 +1,14 @@
 import React from 'react';
-import ordersitem from '../styles/OrderItem.module.css';
+import OrdersList from '../components/OrdersList';
+import style from '../styles/OrderItem.module.css';
 
 const Orders = ({orders}) => {
 return (
-<div className={orders.orders}>
-<h2>Список заказов</h2>
-{orders.map(order => (
-        <OrderItem key={order.id} order={order}/>  
-      ))}
+<div className={style.item}>
+<h2 >Список продацев</h2>
+<OrdersList orders={orders} />
 </div>
 );
 }
 
-const OrderItem = ({order}) => {
-
-    return (
-      <div className={ordersitem.item}>
-        <h3 className={ordersitem.h3}>Поставщик : {order.customer}</h3>  
-        <p className={ordersitem}>Продукт: {order.product}</p>
-        <p className={ordersitem}>Количиство : {order.count}</p>
-        <p className={ordersitem}>Состояние : {order.status}</p>
-        <div className={ordersitem.imageswrapper}>
-        <img className={ordersitem.imageswrapper} src={require(`../../public/${order.image}.png`)} alt={order.product} />
-        </div>
-        
-      </div>
-    )
-  
-  }
-
-
-
-export default Orders
+export default Orders;
