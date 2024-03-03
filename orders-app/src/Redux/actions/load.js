59c54be2b090df7dbff56e49ec54bd6cf01a54bd
Sicor <atrem641@gmail.com>
2024-03-03 13:49:17 +0300

    fix: redux

--- orders-app/src/Redux/actions/load.js
+++ orders-app/src/Redux/actions/load.js
@@ -1,4 +1,15 @@
-import { orders } from '../data';
+const orders = [{
+
+}];
+
+fetch('http://localhost:5000/')
+      .then(response => response.text())
+      .then(data => {
+        orders = data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
 
 export const LOAD_ORDERS = 'LOAD_ORDERS';
 
